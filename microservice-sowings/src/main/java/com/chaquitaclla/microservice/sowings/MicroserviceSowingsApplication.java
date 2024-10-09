468abf8b8aa2ce934e964e3960ef7e491b0dbb33
fdaniel13s <u20221c628@upc.edu.pe>
2024-10-09 11:28:43 -0500

    feat: module added about microservice of sowings.

---
+++ microservice-sowings/src/main/java/com/chaquitaclla/microservice/sowings/MicroserviceSowingsApplication.java
@@ -0,0 +1,13 @@
+package com.chaquitaclla.microservice.sowings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSowingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSowingsApplication.class, args);
+	}
+
+}
---
+++ microservice-sowings/src/test/java/com/chaquitaclla/microservice/sowings/MicroserviceSowingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaquitaclla.microservice.sowings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSowingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
